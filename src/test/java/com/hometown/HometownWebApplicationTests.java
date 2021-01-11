67c2d487b521b0824db6e75aa834934c2e75cbda
qianlanmeng.github.io <960861988@qq.com>
2021-01-11 22:31:14 +0800

    first  创建项目

---
+++ src/test/java/com/hometown/HometownWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hometown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometownWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
